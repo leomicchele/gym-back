a0bdf4153043c4b117e1e64a1d6259e2e41168bf
Leandro Micchele <leomicchele@gmail.com>
2021-08-18 15:00:20 -0300

    Rest-Api Basica v1.0.0

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,36 +1,20 @@
 
 
 function getUsuarios(req, res) {
-   res.json({
-      message: 'Estamos en /api/ - GET'
-   })
+
 }
 
 function postUsuario(req, res) {
-   res.json({
-      message: 'Estamos en /api/ - POST',
-      body: req.body
 
-   })
 }
 
 function patchUsuario(req, res) {
-   res.json({
-      message: 'Estamos en /api/ - PATCH'
-   })
 
 }
 
 function deleteUsuario(req, res) {
-   res.json({
-      message: 'Estamos en /api/ - DELETE'
-   })
 
 }
-
-
-
-
 
 module.exports = {
    getUsuarios,
