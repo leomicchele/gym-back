682689b9de37c067a4663402ec6077e2e17fa9cb
Leandro Micchele <leomicchele@gmail.com>
2021-09-07 13:52:39 -0300

    fix error image

--- public/js/home/subir-imagen.js
+++ public/js/home/subir-imagen.js
@@ -15,7 +15,7 @@ $inputImagen.addEventListener('change', async (event) => {
 
    let estado;
    
-   fetch(`${window.location.origin}api/uploads/usuarios/${id_usuario}`, {
+   fetch(`${window.location.origin}/api/uploads/usuarios/${id_usuario}`, {
       method: 'PUT',
       body: new FormData($formElem)      
    })
