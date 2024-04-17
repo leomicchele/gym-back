dcee544d94405064b8f8d2c52a44757366765fcf
20340237448 <lmicchele@persiscalconsulting.com>
2024-04-17 15:16:59 -0300

    [v0.1] change name variable mongo

--- db/config.js
+++ db/config.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 async function conectandoDB() {
 
    try {
-      await mongoose.connect(process.env.MONGO_DB, {
+      await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
