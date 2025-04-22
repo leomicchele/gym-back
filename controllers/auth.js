8bb2e2f97de9c8ef33052fc275c64c6df2ae1d65
Leandro Gabriel Micchele <leomicchele@gmail.com>
2025-04-22 10:50:37 -0300

    [FIX] Login: Modificación en la búsqueda de alumnos activos en el login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,3 @@
-
 const bcrypt = require('bcrypt');
 const { generadorJWT } = require('../helpers/generador-jwt');
 const verificacionGoogle = require('../helpers/verificacion-google');
@@ -38,7 +37,7 @@ async function login(req, res) {
       
    } else {
       respuesta = await Promise.all([
-         Alumno.findOne({ dni: dni }), // Cuenta a los que estan dados de alta
+         Alumno.findOne({ dni: dni, estado: true }), // Busca solo alumnos activos
          
       ]);
 
@@ -46,9 +45,6 @@ async function login(req, res) {
       if (alumno === null ) {
          return res.status(400).json({ msg: 'Verifica Usuario y contraseña' });
      }
-      if (alumno.estado === false) { // cuando el alumno esta inactivo
-         return res.status(401).json({ msg: 'No autorizado' });         
-      }
    }
 
    let usuario;
@@ -140,45 +136,6 @@ async function login(req, res) {
    
 };
 
-
-// LOGIN CON GOOGLE
-// async function loginGoogle(req, res) {
-//    const tokenGoogle = req.body.id_token
-
-//    try {
-//       // verifica si el token recibido es de Google, y trae los datos del payload
-//      const { email, nombre, apellido, img } = await verificacionGoogle(tokenGoogle);
-
-//       // Verifica si el email del payload existe en nuestra DB
-//       let usuario = await Alumno.findOne({ email: email }) 
-//       if(!usuario) { // Si no existe, lo crea
-//          usuario = new Alumno({email, nombre, apellido, img, password: 'generica', google: true })
-//          await usuario.save()
-//       }
-
-//       // Verifica si el usuario no esta dado de baja. 
-//       if(!usuario.estado) {
-//          res.status(401).json({
-//             msg: 'El usuario esta dado de baja'
-//          })
-//       }
-
-//       // Generar un JWT
-//       const token = await generadorJWT( {id: usuario._id, nombre: usuario.nombre} )
-
-//      res.status(201).json({
-//        msg: "Usuario logiado con GOOGLE",
-//        token: token,
-//        id: usuario._id
-//      });
-
-//    } catch (error) { // Si algo del Try falla, lo recibimos aca.
-      
-//      res.status(400).json({
-//       msg: "No es un token valido de Google",
-//     });
-//    }
-// }
 
 module.exports = {
    login,
